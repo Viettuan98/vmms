89e7997fe6043749af7480b408fec0ed4eb7b904
viettuan <buenotuan98@gmail.com>
2022-08-08 17:36:24 +0700

    update vmms-news

---
+++ vmms-news/js/home-page.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  $('.hot-news-list').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    stagePadding: 0,
+    autoplay: false,
+    responsiveClass: true,
+    responsiveRefreshRate: true,
+    navText: [
+      "<img src='./images/icon-next.svg' alt='Next'>",
+      "<img src='./images/icon-prev.svg' alt='Prev'>",
+    ],
+    responsive: {
+      0: {
+        nav: false,
+      },
+      768: {},
+    },
+  });
+  $('.football-match-list').owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    stagePadding: 0,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsiveRefreshRate: true,
+    navText: [
+      "<img src='./images/icon-next.svg' alt='Next'>",
+      "<img src='./images/icon-prev.svg' alt='Prev'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        margin: 10,
+        stagePadding: 10,
+        autoWidth:true,
+      },
+      768: {
+        items: 2,
+        nav: true,
+      },
+      1200: {
+        items: 3,
+        nav: true,
+      },
+    },
+  });
+  
+});
