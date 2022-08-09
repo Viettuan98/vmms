407e3909df0bb82ee3258e3517fdfaaf0dbc21c4
viettuan <buenotuan98@gmail.com>
2022-08-09 17:33:07 +0700

    update

--- vmms-news/js/home-page.js
+++ vmms-news/js/home-page.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.hot-news-list').owlCarousel({
+  $('.hot-news-list,.football-fixture').owlCarousel({
     items: 1,
     loop: true,
     margin: 10,
@@ -20,6 +20,7 @@ $(document).ready(function () {
       768: {},
     },
   });
+  
   $('.football-match-list').owlCarousel({
     items: 3,
     loop: true,
@@ -50,6 +51,74 @@ $(document).ready(function () {
       },
       1200: {
         items: 3,
+        nav: true,
+      },
+    },
+  });
+  $('.news-category-slider').owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    stagePadding: 0,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsiveRefreshRate: true,
+    navText: [
+      "<img src='./images/icon-next.svg' alt='Next'>",
+      "<img src='./images/icon-prev.svg' alt='Prev'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        margin: 10,
+        stagePadding: 10,
+        autoWidth:true,
+      },
+      768: {
+        items: 2,
+        nav: true,
+      },
+      1200: {
+        items: 2,
+        nav: true,
+      },
+    },
+  });
+  $('.photo-home-list,.backstage-home-list').owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    stagePadding: 0,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsiveRefreshRate: true,
+    navText: [
+      "<img src='./images/icon-next.svg' alt='Next'>",
+      "<img src='./images/icon-prev.svg' alt='Prev'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        margin: 10,
+        stagePadding: 10,
+        autoWidth:true,
+      },
+      768: {
+        items: 2,
+        nav: true,
+      },
+      1200: {
+        items: 4,
         nav: true,
       },
     },
